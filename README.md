e973156b5c38a581e71072708a328f7911867106
samotaie <ext-vitalii.samotaiev@here.com>
2019-10-22 14:57:18 +0300

    LCD and BUTTONS work

---
+++ README.md
@@ -0,0 +1,18 @@
+A0 - btn up
+A1 - btn down
+A5 - LED
+
++5v - LCD
+GND - btns, leds, 
+
+
+D12 lcd
+d11 lcd
+d9  lcd
+D7 - btn left
+D8 - btn right
+d6  lcd
+d5  lcd
+d4  lcd
+d3  lcd
+d2  lcd
--- hotwire.c
+++ hotwire.c
@@ -1,18 +1,22 @@
 #include <LiquidCrystal.h>
 #include <EEPROMex.h>
+#include <Button.h>        //https://github.com/JChristensen/Button
 
 // ---> Cutter Setup
+#define NUMBER_OF_MODELS 6
+#define EEPROM_MODEL_NUMBER_ADDRESS 0
+#define EEPROM_MODEL0_ADDRESS 2
+#define DEFAULT_TEMPERATURE 7
 #define PWM_PIN 9
-#define LED_EEPROM 19
+#define LED_EEPROM 6
+#define ENABLE 7
 // ---> End Cutter Setup
 
 // ---> Buttons definition
-#include <Button.h>        //https://github.com/JChristensen/Button
-
-#define DN_PIN 7           //Connect two tactile button switches (or something similar)
-#define UP_PIN 8           //from Arduino pin 7 to ground and from pin 8 to ground.
-#define P_PIN 14          //Connect two tactile button switches (or something similar)
-#define M_PIN 15          //from Arduino pin A0 to ground and from pin A1 to ground.
+#define DN_PIN 16           //Connect two tactile button switches (or something similar)
+#define UP_PIN 17           //from Arduino pin 7 to ground and from pin 8 to ground.
+#define P_PIN 15          //Connect two tactile button switches (or something similar)
+#define M_PIN 14          //from Arduino pin A0 to ground and from pin A1 to ground.
 #define PULLUP true        //To keep things simple, we use the Arduino's internal pullup resistor.
 #define INVERT true        //Since the pullup resistor will keep the pin high unless the
                            //switch is closed, this is negative logic, i.e. a high state
@@ -24,6 +28,7 @@
 #define MIN_COUNT 0
 #define MAX_COUNT 255
 #define MAX_COUNTBI 5
+#define EEPROM_LOOP 10000
 
 Button btnUP(UP_PIN, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the buttons
 Button btnDN(DN_PIN, PULLUP, INVERT, DEBOUNCE_MS);
@@ -43,50 +48,133 @@ int lastCountbi = -1;                   //Previous value of count (initialized t
 unsigned long rptbi = REPEAT_FIRST;     //A variable time that is used to drive the repeats for long presses
 // ---> End of Buttons definition
 
+int prevent_first_write =0;
+int actual =0;
+int bright=0;
+int enable=0;
+unsigned long hold=0;
+unsigned long unhold=0;
+
 // ---> EEPROM def and hotwire related stuff
 //LiquidCrystal lcd(rs, e, d5, d4, d3, d2);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 // eeprom start address
-int address = 0;
-int input = 255; // default value for arduino EEPROM wire temp.
+int model_num = 0;
+int address = EEPROM_MODEL0_ADDRESS+2*model_num;
+int current_temperature =0;
 int output = 0;
 int val=0;
 int cls=1;
+int temperature[6]; // massive that stores temperature value for each model
+int temperaturechenge[6]; // massive that stores temperature value for each model
+int modelchanged = 0;
 // ---> End of EEPROM def and hotwire related stuff
 
 void setup(void)
 {
-    //Serial.begin(115200);
     Serial.begin(9600);  
     Serial.println();
     // ---> Cutter Setup
+    Serial.println("Setup IO Ports");
+    Serial.println();
     pinMode(PWM_PIN, OUTPUT);
     analogWrite(PWM_PIN, 0);
     pinMode(LED_EEPROM, OUTPUT);
+    pinMode(ENABLE, INPUT);
+    digitalWrite(ENABLE, HIGH); 
     // ---> End Cutter Setup
 
     // ---> EEPROM def and hotwire related stuff
-    // Определение количество символов в строке и количество строк:
+    // Set number of laters in the string
+    Serial.println("Init LCD");
+    Serial.println();
     lcd.begin(8, 2);
     lcd.print("TeamSeal");
     //Далее перемещаемся в начало второй строки
     lcd.setCursor(0,1);
-    //int a=0;
-    for (int a=0; a<8; a++) { delay(200);lcd.print("-"); }
-    lcd.setCursor(0,0); lcd.print("Mat Temp"); lcd.setCursor(0,1); lcd.print("        ");
-    // Uncomment next line to write first value to EEPROM and 
-    // comment it back when switch to new Arduino
-    //EEPROM.writeInt(address, input);
-    EEPROM.setMaxAllowedWrites(10); // STOP people of rewriting EEPROM more than 10 times
-    output = EEPROM.readInt(address);    digitalWrite(LED_EEPROM, HIGH); delay(200);digitalWrite(LED_EEPROM, LOW);
-    int addressInt       = EEPROM.getAddress(sizeof(int));
-        Serial.print(addressInt);       Serial.print(" \t\t\t "); Serial.print(sizeof(int));  Serial.println(" (int)");
-
-    //if (output != 0) {EEPROM.writeInt(address, input);}
-    val=output;
-    count=output;
+    for (int a=0; a<50; a++) { 
+        delay(30);
+        // lcd.print("-");
+        analogWrite(LED_EEPROM, a);
+        switch (a) {
+            case 7: lcd.print("W");break;
+            case 14: lcd.print("a");break;   //execution starts at this case label
+            case 21: lcd.print("r");break;
+            case 28: lcd.print("m");break;
+            case 35: lcd.print("i");break;
+            case 42: lcd.print("n");break;
+            case 49: lcd.print("g"); delay(400);break;
+        }
+    }
+    analogWrite(LED_EEPROM, 0);
+    lcd.clear();
+    lcd.setCursor(0,0); lcd.print("Type t"); lcd.setCursor(0,1); lcd.print("        ");
+    EEPROM.setMaxAllowedWrites(20); // STOP people of rewriting EEPROM more than 10 times
+    Serial.println("Read Values from EEPROM");
+    Serial.println();
+    // Reading model number
+    model_num = EEPROM.readInt(EEPROM_MODEL_NUMBER_ADDRESS);
+    Serial.println("CurrentModel");
+    Serial.println(model_num);
+    Serial.println();
+    current_temperature = EEPROM.readInt(EEPROM_MODEL0_ADDRESS+2*model_num);
+    Serial.println("CurrentTemp");
+    Serial.println(current_temperature);
+    Serial.println();
+    // Zeroing model number
+    if(model_num < 0 || model_num > NUMBER_OF_MODELS) {
+        Serial.println("Model number out of Range. Setting model_num to 0");
+        Serial.println();
+        model_num=0;
+        // EEPROM.writeInt(EEPROM_MODEL_NUMBER_ADDRESS, model_num);
+        analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);delay(100);
+        analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);delay(100);
+        analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);
+    }
+    int address=EEPROM_MODEL0_ADDRESS;
+    // Reading model temperatures
+    for(int i=0; i<NUMBER_OF_MODELS; i++) {
+        address=EEPROM_MODEL0_ADDRESS+2*i;
+        temperature[i]=EEPROM.readInt(address);
+        temperaturechenge[i]=0;
+        Serial.print("Model Numder:"); Serial.print(i); Serial.print("  ");
+        Serial.print("Temp:"); Serial.print(temperature[i]);
+        Serial.println();
+    }
+    // Zeroing model temperatures
+    address=EEPROM_MODEL0_ADDRESS;
+    for(int i=0; i<NUMBER_OF_MODELS; i++) {
+        address=EEPROM_MODEL0_ADDRESS+2*i;
+        if (temperature[i]<0 || temperature[i]>255) {
+            Serial.println("Temperature is out of range. Setting temperature to default value");
+            Serial.println();
+            EEPROM.writeInt(address, DEFAULT_TEMPERATURE);
+            digitalWrite(LED_EEPROM, HIGH); delay(150);digitalWrite(LED_EEPROM, LOW);delay(150);
+            digitalWrite(LED_EEPROM, HIGH); delay(150);digitalWrite(LED_EEPROM, LOW);delay(150);
+            digitalWrite(LED_EEPROM, HIGH); delay(150);digitalWrite(LED_EEPROM, LOW);delay(150);
+        }
+    }
+//    int addressInt       = EEPROM.getAddress(sizeof(int));
+//    Serial.print(addressInt);       Serial.print(" \t\t\t "); Serial.print(sizeof(int));  Serial.println(" (int)");
+    lcd.setCursor(0,1);
+    switch (model_num) {
+            case 0: lcd.print("Foam");break;
+            case 1: lcd.print("Foa+");break;   //execution starts at this case label
+            case 2: lcd.print("EPP");break;
+            case 3: lcd.print("EPP+");break;
+            case 4: lcd.print("EPO");break;
+            case 5: lcd.print("EPO+");break;
+        }
+    // if(model_num==0) {lcd.print("Foam");}
+    // if(model_num==1) {lcd.print("Foa+");}
+    // if(model_num==2) {lcd.print("EPP");}
+    // if(model_num==3) {lcd.print("EPP+");}
+    // if(model_num==4) {lcd.print("EPO");}
+    // if(model_num==5) {lcd.print("EPO+");}
+    countbi=model_num; // 5
+    count=temperature[model_num]; // 7
     lcd.setCursor(5,1);
-    lcd.print(output);
+    lcd.print(temperature[model_num]);
     // ---> End of EEPROM def and hotwire related stuff
 }
 
@@ -96,26 +184,71 @@ void loop(void)
     btnDN.read();
     btnP.read();
     btnM.read();
+    address=EEPROM_MODEL0_ADDRESS+model_num*2;
+    
     if (count != lastCount || countbi != lastCountbi) {                 //print the count if it has changed
-        lastCount = count; lastCountbi=countbi;
-//        Serial.println(count, DEC);
-        static const unsigned long REFRESH_INTERVAL = 300; // ms
-        static unsigned long lastRefreshTime = 0;  
-        if(millis() - lastRefreshTime >= REFRESH_INTERVAL)
-        {
+        if (countbi != lastCountbi && prevent_first_write ==1){
+            Serial.println("Switching to model number");
+            Serial.println(countbi);
+            Serial.println();
+            Serial.println("Reading model temperature");
+            Serial.println(temperature[model_num]);
+            Serial.println();
+            modelchanged =1;
+            model_num=countbi;
+            address=EEPROM_MODEL0_ADDRESS+model_num*2;
+            //temperature[model_num]=EEPROM.readInt(address);
+            // Updating count value
+            count=temperature[model_num];
+            lastCount = count;
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Type t");
+            lcd.setCursor(0,1);
+            if(model_num==0) {lcd.print("Foam");}
+            if(model_num==1) {lcd.print("Foa+");}
+            if(model_num==2) {lcd.print("EPP");}
+            if(model_num==3) {lcd.print("EPP+");}
+            if(model_num==4) {lcd.print("EPO");}
+            if(model_num==5) {lcd.print("EPO+");}
+            // Updating last used model
+            Serial.println("WRITING EEPROM!");
+            EEPROM.updateInt(EEPROM_MODEL_NUMBER_ADDRESS, model_num); // run it rarely
+            actual=count;
+            EEPROM.updateInt(address, actual);
+            digitalWrite(LED_EEPROM, HIGH); digitalWrite(LED_EEPROM, LOW);
+        }
+        if (count != lastCount && prevent_first_write ==1) {
+            temperature[model_num]=count;
+            actual=count;
+            temperaturechenge[model_num]=1;
+            Serial.println("updating model temperature");
+            Serial.println(temperature[model_num]);
+            Serial.println();
+            //EEPROM.updateInt(address, temperature[model_num]);
+        }
+
+        lastCount = count; 
+        lastCountbi=countbi;
+    static const unsigned long REFRESH_INTERVAL = 300; // ms
+        static unsigned long lastRefreshTime = 0;
+        if(millis() - lastRefreshTime >= REFRESH_INTERVAL) {
             lastRefreshTime += REFRESH_INTERVAL;    
-            if (count==99 || count==9 || countbi != lastCountbi) {cls=1;}
-            if (cls=1) {lcd.clear();lcd.print("Mat Temp"); cls=0;}
+            if (temperature[model_num]==99 || temperature[model_num]==9) {
+                lcd.clear();
+                lcd.setCursor(0,0);
+                lcd.print("Type t");
+                lcd.setCursor(0,1);
+                if(model_num==0) {lcd.print("Foam");}
+                if(model_num==1) {lcd.print("Foa+");}
+                if(model_num==2) {lcd.print("EPP");}
+                if(model_num==3) {lcd.print("EPP+");}
+                if(model_num==4) {lcd.print("EPO");}
+                if(model_num==5) {lcd.print("EPO+");}
+                Serial.println("clearing screen");
+            }
             lcd.setCursor(5,1);
-            lcd.print (count);
-
-            if(countbi==0) {lcd.setCursor(0,1); lcd.print("Foam"); EEPROM.updateInt(address, count); digitalWrite(LED_EEPROM, HIGH);digitalWrite(LED_EEPROM, LOW); } 
-            if(countbi==1) {lcd.setCursor(0,1); lcd.print("Foa+");} 
-            if(countbi==2) {lcd.setCursor(0,1); lcd.print("EPO");} 
-            if(countbi==3) {lcd.setCursor(0,1); lcd.print("EPO+");} 
-            if(countbi==4) {lcd.setCursor(0,1); lcd.print("EPP");} 
-            if(countbi==5) {lcd.setCursor(0,1); lcd.print("EPP+");}
-
+            lcd.print(temperature[model_num]); 
         }
     }
     
@@ -176,5 +309,49 @@ void loop(void)
             STATEBI = WAIT;
             break;
     }
-    analogWrite(PWM_PIN, count );
+    enable=digitalRead(ENABLE);
+    // delay(500);
+    if (enable==1)    {
+        hold=millis();
+        analogWrite(PWM_PIN, count );
+        if (count >0 && count < 17)     {bright=1;}
+        else if(count >=17)             { bright = map(count, 0, 255, 0, 16); }
+        else                            { bright = 0; }
+        analogWrite(LED_EEPROM, bright);
+    }
+    else {
+        analogWrite(PWM_PIN, 0 );
+        analogWrite(LED_EEPROM, 0);
+        if (hold-unhold >= 400 && hold-unhold <= 1000) {
+            
+            if (modelchanged ==1){
+                Serial.println("Writing new model number");
+                Serial.println(model_num);
+                modelchanged=0;
+                EEPROM.writeInt(EEPROM_MODEL_NUMBER_ADDRESS, model_num);
+                analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);delay(100);
+                analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);delay(100);
+            }
+            for (int v=0;v<NUMBER_OF_MODELS;v++) {
+                if (temperaturechenge[v]==1) {
+                    Serial.print("Writing new temperature for model ");
+                    Serial.print(v);
+                    Serial.print(" ");
+                    Serial.print("Temperature=");
+                    Serial.print(temperature[v]);
+                    address = EEPROM_MODEL0_ADDRESS+2*v;
+                    temperaturechenge[v]=0;
+                    Serial.print("ModelAddress = ");
+                    Serial.print(address);
+                    Serial.println("");
+                    EEPROM.writeInt(address, temperature[v]);
+                    analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);delay(100);
+                    analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);delay(100);
+                    analogWrite(LED_EEPROM, 25); delay(100);analogWrite(LED_EEPROM, 0);
+                }
+            }
+        }
+        unhold=millis();
+    }
+    prevent_first_write =1;
 }
